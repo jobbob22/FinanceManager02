e9fec0f960ef35d2909c91a3800cb50192221986
job assis <jobbob21teacher@gmail.com>
2025-01-14 16:06:14 -0300

    Criação das duas classes e inicio do preenchimento dos metodos

---
+++ FinanceManager/FinanceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager
+{
+    public class FinanceManager
+    {
+        private List<Transaction> transactions = new List<Transaction>();
+
+        public void AddTransaction(string descricao, decimal valor, DateTime? data)
+        {
+            var transaction = new Transaction(descricao, valor, data);
+
+            transactions.Add(transaction);
+
+        }
+        public void ListTransactions()
+        {
+            foreach (var transaction in transactions)
+
+            {
+
+                Console.WriteLine($"Descrição: {transaction.Descricao}, Valor: {transaction.Valor}, Data: {transaction.Data}");
+
+            }
+
+        }
+    }
+}
---
+++ FinanceManager/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager
+{
+    public class Transaction
+    {
+        public Transaction(string descricao, decimal valor, DateTime? data)
+        {
+            Descricao = descricao;
+            Valor = valor;
+            Data = data;
+        }
+
+       public  string Descricao {  get; set; }
+        public decimal Valor {  get; set; }
+        public DateTime? Data { get; set; }
+
+    }
+}
